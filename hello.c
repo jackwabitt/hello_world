a4fd32c2e6324b82a733af053bdbb706e6c373c2
jackwabitt <48189751+jackwabitt@users.noreply.github.com>
2020-02-11 08:43:34 -0500

    Create hello.c
    Testing123
    Actively Making Possible

---
+++ hello.c
@@ -0,0 +1,11 @@
+/* cd <DIRECTORY_WITH_FILE_TO_COMPILE>
+ * g++ <FILE_NAME.c> -o <EXEC_NAME>
+ * LEARN IT, LIVE IT, LOVE IT 4HEAD
+ */
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World from c!");
+   return 0;
+}
